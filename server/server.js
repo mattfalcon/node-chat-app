68573d2f14b0a1ba0d07eb86ba61cefbcf2a8f8b
Matt Valladarez <Matt Valladarez>
2017-06-23 22:38:56 -0500

    initial files

---
+++ server/server.js
@@ -0,0 +1,21 @@
+//built in module
+const path = require('path');
+//load in express
+const express = require('express');
+
+//avoids going into and out of server
+const publicPath = path.join(__dirname, '../public');
+
+//
+const port = process.env.PORT || 3000;
+
+//configure express by calling methods on app
+var app = express();
+
+//calling app to configue express static middleware
+app.use(express.static(publicPath));
+
+//start up server on port 3000 with callback function (console.log)
+app.listen(3000, () => {
+    console.log(`Server is up on ${port}`);
+});
